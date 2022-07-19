4280c37438841b39a015506d07837be8c1f16b5a
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-07-19 15:20:21 +0530

    auth created

--- functions/roles/index.js
+++ functions/roles/index.js
@@ -5,31 +5,43 @@ const bcrypt = require("bcrypt");
 
 // Create user
 router.post('/v2/register', async (req, res) => {
-    try {
-    let password = req.body.password;
-    const cpassword = req.body.cpassword;
-    if(password === cpassword){
-        password = await bcrypt.hash(password,10);
-        const postDATA = await db.collection('roles').add({
-            password: password,
-            email: req.body.email,
-            name: req.body.name,
-            role: req.body.role,
-            createdAt: new Date()
+    const email = req.body.email;
+    const snapshot = await db.collection('roles').where('email', '==', email).get();
+    const docs = [];
+    snapshot.forEach(doc => {
+        docs.push({ id: doc.id, ...doc.data() });
+      });
+    if(docs[0]?.email === email){
+        res.status(400).send({
+            message: "User already exists, please login"
         });
-        return res.status(200).json({
-            message: "Acc successfully created",
-          });
     }else{
-        return res.status(400).json({
-            message: "Passwords do not match",
-          });
-    }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message: "Something went wrong",
-          });
+        try {
+            let password = req.body.password;
+            const cpassword = req.body.cpassword;
+            if(password === cpassword){
+                password = await bcrypt.hash(password,10);
+                const postDATA = await db.collection('roles').add({
+                    password: password,
+                    email: req.body.email,
+                    name: req.body.name,
+                    role: req.body.role,
+                    createdAt: new Date()
+                });
+                return res.status(200).json({
+                    message: "Acc successfully created",
+                  });
+            }else{
+                return res.status(400).json({
+                    message: "Passwords do not match",
+                  });
+            }
+            } catch (error) {
+                console.log(error);
+                return res.status(500).json({
+                    message: "Something went wrong",
+                  });
+            }
     }
 });
 // login user
@@ -46,6 +58,7 @@ router.post('/v2/login', async (req, res) => {
     if(varifyPass){
         res.status(200).json({
             message: "Login Successful",
+            user: docs
           });
     }else{
         res.status(404).json({
--- functions/roles/isAuth.js
+++ functions/roles/isAuth.js
@@ -1,17 +1,31 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 const isAuthenticated = (req, res, next) => {
-  const authHeader = req.headers["authorization"] || req.cookies.jwt;
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.sendStatus(401);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
-    }
-
-    req.user = user;
-    next();
-  });
-};
+  try {
+      // const authHeader = req.headers["authorization"] || req.cookies.jwt;
+      // const token = authHeader && authHeader.split(" ")[1];
+      const cookie = req.cookies.jwt;
+      if (!cookie) return res.sendStatus(401);
+      const tokenVarify = jwt.verify(cookie,process.env.SECRET_KEY, async (err, user) => {
+        if (err) {
+          return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+      });
+      const snapshot = await db.collection('roles').where('_id', '==', tokenVarify._id).get();
+      const docs = [];
+      snapshot.forEach(doc => {
+          docs.push({ id: doc.id, ...doc.data() });
+      });
+      req.user = docs[0];
+      req.cookie = cookie;
+      next()
+  } catch (error) {
+    res.status(200).send(error)
+  }
+}
 
 module.exports = isAuthenticated;
+
