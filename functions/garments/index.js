ee73e51b819a9f5d2828c5fd1c86a9b5dc27d197
lirajkhanna <lirajkhanna2002@gmail.com>
2022-07-25 23:05:36 +0530

    Garments return data

--- functions/garments/index.js
+++ functions/garments/index.js
@@ -11,10 +11,19 @@ router.post("/v2/post", async (req, res) => {
       garment_details: req.body.garment_details,
       stitching_category: req.body.stitching_category,
     });
+
+    const prevDoc = db.collection("garments").doc(postDATA._path.segments[1]);
+    const queries = await prevDoc.get();
+    const getDATA = queries.data();
+
+    getDATA.id=postDATA._path.segments[1];
+
+
     return res.status(200).send(
       JSON.stringify({
         message: "Garment details posted successfully",
-        data: postDATA,
+        data: getDATA,
+        postData: postDATA,
       })
     );
   } catch (error) {
@@ -26,10 +35,10 @@ router.post("/v2/post", async (req, res) => {
 //Update
 router.put("/v2/put/:id", async (req, res) => {
   try {
-    const prevDoc = db.collection("garments").doc(req.params.id);
-    const queries = await prevDoc.get();
-    const getDATA = queries.data();
-
+    let prevDoc = db.collection("garments").doc(req.params.id);
+    let queries = await prevDoc.get();
+    let getDATA = queries.data();
+    
     const document = db.collection("garments").doc(req.params.id);
     const updateDATA = await document.update({
       category: req.body.category || getDATA.category,
@@ -38,10 +47,19 @@ router.put("/v2/put/:id", async (req, res) => {
       stitching_category:
         req.body.stitching_category || getDATA.stitching_category,
     });
+
+    prevDoc = db.collection("garments").doc(req.params.id);
+    queries = await prevDoc.get();
+    getDATA = queries.data();
+
+    getDATA.id=req.params.id;
+
+
     return res.status(200).send(
       JSON.stringify({
         message: "Garment details updated successfully",
-        data: updateDATA,
+        data: getDATA,
+        updateData: updateDATA,
       })
     );
   } catch (error) {
