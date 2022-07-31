14746632738331df87fc857bc718de6a7051ef17
lirajkhanna <lirajkhanna2002@gmail.com>
2022-07-31 22:09:08 +0530

    SMS for customers and orders by masterji

--- functions/twilio.js
+++ functions/twilio.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const functions = require("firebase-functions");
 
-let config = require("../env.json");
+let config = require("./env.json");
 
 if(Object.keys(functions.config()).length){
   config=functions.config();
 }
 
 const accountSid = config.service.twilio_account_sid;
-const authToken = config.service.auth_token;
+const authToken = config.service.twilio_auth_token;
 
 const sendSms = (phone, message) => {
   const client = require("twilio")(accountSid, authToken);
